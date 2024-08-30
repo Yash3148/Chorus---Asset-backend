0c96cc374f2a734dbf104e2472d82991ad6a9ca1
Vikrant Giri <vikrant.giri@qodequaytech.com>
2024-08-30 20:47:44 +0530

    added group by

--- src/assets/assets.service.ts
+++ src/assets/assets.service.ts
@@ -58,5 +58,9 @@ export class AssetsService {
 
   async getGroupBy(groupBy: GroupByFilterDto): Promise<any> {
     const { location, description } = groupBy;
+    // const total = await this.assetRepository.groupByDescription();
+    // const monitored = await this.assetRepository.countNotUnableToLocate();
+    const res = await this.assetRepository.groupByDescriptionWithStatusCount();
+    return res;
   }
 }
